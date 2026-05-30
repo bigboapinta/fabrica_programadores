deaeb8e75ecc2714a3f344a16fb1d6311c1db170
bigboapinta <catianedossantos65@gmail.com>
2026-05-30 16:58:26 -0300

    big zinhuu

---
+++ 06-loop-while-py/desafio3.py
@@ -0,0 +1,40 @@
+#
+soma = 0
+i = 0
+
+while i < 4:
+    nota = float(input(f"Digite a nota {i+1}: "))
+    soma += nota
+    i += 1
+
+media = soma / 4
+
+print("Média:", media)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 06-loop-while-py/desafio4.py
@@ -0,0 +1,26 @@
+# 
+
+soma = 0
+
+for i in range(4):
+    nota = float(input(f"Digite a nota {i+1}: "))
+    soma += nota
+
+media = soma / 4
+
+print("Média:", media)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
