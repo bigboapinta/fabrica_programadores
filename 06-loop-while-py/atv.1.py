4d2c6c05740c7df1ff816ba505394ad665c294e5
bigboapinta <catianedossantos65@gmail.com>
2026-05-30 16:27:42 -0300

    apenas eu ne big boa pinta

---
+++ 06-loop-while-py/atv.1.py
@@ -0,0 +1,16 @@
+#
+numero = int(input("Digite um número para ver a tabuada: "))
+i = int(input("Digite até qual número você quer ver a tabuada: "))
+f = int(input("Digite o número de vezes que você quer ver a tabuada: "))
+while i <= f :
+    print(f"{numero} x {i} = {numero  * i }")
+    i += 1
+   
+
+
+
+
+
+
+
+
